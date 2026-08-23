fd21ddbd0df245f8c42096f2e9e02ad7e980bd81
sayuveos11 <kkallinberg@gmail.com>
2026-08-23 13:36:07 +0300

    Add binary search and selection sort folders

---
+++ binary search/example.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_index = i
+
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return arr
+
+numbers = [5, 8, 3, 2, 4]
+print(selection_sort(numbers))
